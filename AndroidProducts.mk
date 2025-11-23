98d170be0a461cde1981c19334f9ed96ade682c2
omarltxy5 <omarkashkoush122@gmail.com>
2025-11-23 18:14:09 +0200

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for motorola paros (paros)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_paros.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_paros-user \
+    omni_paros-userdebug \
+    omni_paros-eng
